25a1ba862e60f52d8159da8830a403a0c9a89850
Nícolas D. Schmidt <nicolasdschmidt@gmail.com>
2021-06-15 00:15:07 -0300

    dev: setup typescript and linting
    setup:
    - typescript
    - eslint, prettier
    - editorconfig

---
+++ pages/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Head from 'next/head'
+
+const Home: React.FC = () => {
+	return (
+		<div>
+			<Head>
+				<title>Blog</title>
+			</Head>
+
+			<main>
+				<h1>Blog</h1>
+			</main>
+		</div>
+	)
+}
+
+export default Home
