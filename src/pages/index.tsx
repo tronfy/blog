8f849408eb3ddca912020d2c8157c2ccb0897cdf
Nícolas D. Schmidt <nicolasdschmidt@gmail.com>
2021-06-15 19:08:24 -0300

    chore: use sass on style modules

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Head from 'next/head'
 
-import styles from '../styles/Home.module.css'
+import styles from '../styles/Home.module.scss'
 import TextBob from '../components/TextBob'
 
 const Home: React.FC = () => {
