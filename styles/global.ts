a6b5bd0904fe1d6acacb85db3262de15136f300a
Nícolas D. Schmidt <nicolasdschmidt@gmail.com>
2021-06-17 13:47:19 -0300

    chore: add theme

--- styles/global.ts
+++ styles/global.ts
@@ -5,22 +5,47 @@ const GlobalStyle = createGlobalStyle`
 		margin: 0;
 		padding: 0;
 		box-sizing: border-box;
+		//outline: 1px solid magenta;
 	}
 
 	body {
+		display: block;
 		background: ${props => props.theme.colors.background};
 		color: ${props => props.theme.colors.text};
-		font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+
+		font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+		font-size: 14pt;
+		text-align: justify;
+		text-justify: auto;
 	}
 
-	body a {
+	h1 {
+		font-size: 28pt;
+	}
+
+	h2 {
+		font-size: 20pt;
+	}
+
+	h3 {
+		font-size: 16pt;
+	}
+
+	a {
 		color: ${props => props.theme.colors.primary};
+		//text-decoration: underline 1.5pt solid;
+		//text-underline-offset: 2pt;
 		text-decoration: none;
 	}
 
-	body a:hover {
+	a:hover {
 		color: ${props => props.theme.colors.background};
 		background: ${props => props.theme.colors.primary};
+		text-decoration: none;
+	}
+
+	li {
+		list-style: none;
 	}
 
 	::selection {
