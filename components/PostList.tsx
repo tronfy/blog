99eeeea62160a59965445993edcda56d9bab6582
Nícolas D. Schmidt <nicolasdschmidt@gmail.com>
2021-06-17 14:04:19 -0300

    chore: make PostList into a component

---
+++ components/PostList.tsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import { Post } from '../types'
+import styles from '../styles/PostList.module.scss'
+import { readingTime } from '../scripts/readingTime'
+
+type Props = {
+	posts: Post[]
+}
+
+const PostList: React.FC<Props> = props => {
+	return (
+		<section>
+			<ul>
+				{props.posts.map((post, idx) => (
+					<a href="#" key={idx}>
+						<li>
+							<div className={styles.post_item}>
+								<div className={styles.post_item_header}>
+									<span>{post.frontmatter.date}</span>
+									<span> &#8226; </span>
+									<span>{readingTime(post)} min read</span>
+								</div>
+								<h3>{post.frontmatter.title}</h3>
+								<p>{post.frontmatter.description}</p>
+								<div className={styles.post_item_footer}>
+									{post.frontmatter.tags
+										.sort()
+										.map((tag, idx) => (
+											<span key={idx}>
+												<span>{tag}</span>
+												{idx <
+												post.frontmatter.tags.length -
+													1 ? (
+													<span> &#8226; </span>
+												) : (
+													''
+												)}
+											</span>
+										))}
+								</div>
+							</div>
+						</li>
+					</a>
+				))}
+			</ul>
+		</section>
+	)
+}
+
+export default PostList
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,8 +3,8 @@ import React from 'react'
 import Head from '../components/Head'
 import styles from '../styles/Home.module.scss'
 import { getAllPosts } from '../scripts/getAllPosts'
-import { readingTime } from '../scripts/readingTime'
 import { Post } from '../types'
+import PostList from '../components/PostList'
 
 type Props = {
 	posts: Post[]
@@ -24,53 +24,7 @@ const Home: React.FC<Props> = props => {
 			</header>
 
 			<main className={styles.main}>
-				<section>
-					<ul>
-						{props.posts.map((post, idx) => (
-							<a href="#" key={idx}>
-								<li>
-									<div className={styles.post_item}>
-										<div
-											className={styles.post_item_header}
-										>
-											<span>{post.frontmatter.date}</span>
-											<span> &#8226; </span>
-											<span>
-												{readingTime(post)} min read
-											</span>
-										</div>
-										<h3>{post.frontmatter.title}</h3>
-										<p>{post.frontmatter.description}</p>
-										<div
-											className={styles.post_item_footer}
-										>
-											{post.frontmatter.tags
-												.sort()
-												.map((tag, idx) => (
-													<>
-														<span key={idx}>
-															{tag}
-														</span>
-														{idx <
-														post.frontmatter.tags
-															.length -
-															1 ? (
-															<span>
-																{' '}
-																&#8226;{' '}
-															</span>
-														) : (
-															''
-														)}
-													</>
-												))}
-										</div>
-									</div>
-								</li>
-							</a>
-						))}
-					</ul>
-				</section>
+				<PostList posts={props.posts} />
 			</main>
 		</div>
 	)
