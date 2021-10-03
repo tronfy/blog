9b6132a3f0a6dc04f1f61a40df11c5b46ab97aa4
Nícolas D. Schmidt <nicolasdschmidt@gmail.com>
2021-10-03 00:36:34 -0300

    chore: make sitemap google-compliant

--- scripts/sitemap.ts
+++ scripts/sitemap.ts
@@ -7,11 +7,22 @@ import { getAllPostMetas } from './getPosts'
 const genSitemap = async (): Promise<void> => {
   const smStream = new SitemapStream({
     hostname: `https://nds.dev.br`,
+    xmlns: {
+      news: true,
+      xhtml: true,
+      image: true,
+      video: true,
+      custom: [
+        'xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd"',
+        'xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"',
+      ],
+    },
   })
 
   // home
   smStream.write({
     url: `/`,
+    lastmod: new Date(),
     changefreq: 'weekly',
     priority: 1,
   })
@@ -19,6 +30,7 @@ const genSitemap = async (): Promise<void> => {
   // about
   smStream.write({
     url: `/sobre`,
+    lastmod: new Date(),
     changefreq: 'weekly',
     priority: 0.8,
   })
@@ -28,6 +40,7 @@ const genSitemap = async (): Promise<void> => {
   posts.forEach(post => {
     smStream.write({
       url: `/blog/${post.slug}/`,
+      lastmod: new Date(post.frontmatter.date),
       changefreq: 'daily',
       priority: 0.9,
     })
