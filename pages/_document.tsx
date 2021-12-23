9c7290317f78f582d61ef70bb6170026cdf0a25b
Nícolas D. Schmidt <nicolasdschmidt@gmail.com>
2021-12-23 14:34:06 -0300

    fix: link light fonts

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -46,7 +46,7 @@ export default class MyDocument extends Document {
 
           <link
             rel="stylesheet"
-            href="https://fonts.googleapis.com/css?family=Roboto:400,700&display=optional"
+            href="https://fonts.googleapis.com/css?family=Roboto:300,400,700&display=optional"
           />
 
           <link
